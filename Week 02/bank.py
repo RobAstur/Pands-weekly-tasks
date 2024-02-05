597ebda05db3cf61548ef95c00c806ffe6c3dd22
Roberto Gomez Garcia <gomezgar.roberto@gmail.com>
2024-02-05 12:01:25 +0000

    week 02 - bank

---
+++ Week 02/bank.py
@@ -0,0 +1,9 @@
+#Weekly task 02
+#Author: roberto Gomez Garcia
+#Tip: All the numbers are considered cents. 
+
+name = input("Please advise your name ")
+first_number = int(input("Enter amount in cents: "))
+second_number = int(input("Enter amount in cents: "))
+
+print (f'User:', name,'-', "Total amount = ","€",(first_number + second_number)/100)
